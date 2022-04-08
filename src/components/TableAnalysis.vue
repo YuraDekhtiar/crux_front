93912cabd67b074fb384dab5f55573d0f657ca74
diana <dianakorjagina550@gmail.com>
2022-04-08 19:01:05 +0300

    added icons for form factor

--- src/components/TableAnalysis.vue
+++ src/components/TableAnalysis.vue
@@ -41,7 +41,19 @@
     <!-- Loop through the list get the each student data -->
     <tr v-for="item in filteredList" :key='item'>
       <td><input type="checkbox" v-bind:value='item.url_id' v-model="checked" v-on:change="checkUpdate()" ></td>
-      <td class="text-left" v-for="field in fields" :key='field'>{{item[field]}}</td>
+      <td class="text-left p-2">{{ item.url }}</td>
+
+      <td v-if="item.form_factor=='phone'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+        <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+        <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+      </svg></td>
+      <td v-else ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
+        <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
+      </svg></td>
+      <td class="text-left p-2">{{ Math.round(item.good*100)+'%'}}</td>
+      <td class="text-left p-2">{{ Math.round(item.needs_improvement*100)+'%' }}</td>
+      <td class="text-left p-2">{{ Math.round(item.poor*100)+'%' }}</td>
+<!--      <td class="text-left" v-for="field in fields" :key='field'>{{item[field]}}</td>-->
     </tr>
     </tbody>
   </table>

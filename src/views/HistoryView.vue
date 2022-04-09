7e060011779afb85a45430747a12a73e4c01856a
yura <yura.zb.ua@gmail.com>
2022-04-09 17:21:34 +0300

    history page fixed css

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,8 +1,4 @@
 <template>
-  <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" >
-  </head>
-
   <div class="container text-center">
     <div class="table-responsive ">
       <TableHistory :fields='fields' :urlData ="[...responseData]" :fieldsName="fieldsName"></TableHistory>
